e0e992ce3046775f4b3f7b05b199879a9b56bb68
warior4356 <warior4356@yahoo.com>
2021-02-10 19:10:17 -0800

    Adds example cfg.py

---
+++ cfg_example.py
@@ -0,0 +1,7 @@
+token = "Discord API token"
+db_password = "Secret Password"
+polling_interval = 60  # Seconds
+alert_channel = 000000000000000000  # Discord channel ID
+list_channel = 000000000000000000  # Discord channel ID
+first_interval = 3600  # Seconds
+second_interval = 900  # Seconds
